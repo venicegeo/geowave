0328b9fec8f6e528dcf3536bde19c72b47c78ef8
Grant Soyka <gsoyka@gmail.com>
2018-01-04 14:48:41 -0700

    Change comparison operator

--- deploy/packaging/docker/build-src/build-geowave-vendor.sh
+++ deploy/packaging/docker/build-src/build-geowave-vendor.sh
@@ -67,7 +67,7 @@ mvn -q package -am -pl deploy -P hbase-container-singlejar -Dhbase.finalName=geo
 mvn -q package -am -pl deploy -P geowave-tools-singlejar -Dtools.finalName=geowave-tools-${GEOWAVE_VERSION}-${VENDOR_VERSION} $BUILD_ARGS "$@"
 
 # Build Accumulo API Jars
-if [[ "$ACCUMULO_API" -ne "1.7" ]]; then
+if [[ "$ACCUMULO_API" != "1.7" ]]; then
   mvn -q package -am -pl deploy -P geowave-tools-singlejar -Dtools.finalName=geowave-tools-${GEOWAVE_VERSION}-${VENDOR_VERSION}-accumulo1.7 $BUILD_ARGS "$@" -Daccumulo.version=1.7.2 -Daccumulo.api=1.7
   mvn -q package -am -pl deploy -P accumulo-container-singlejar -Dtools.finalName=geowave-accumulo-${GEOWAVE_VERSION}-${VENDOR_VERSION}-accumulo1.7 $BUILD_ARGS "$@" -Daccumulo.version=1.7.2 -Daccumulo.api=1.7
   mvn -q package -am -pl services/rest -P rest-services-war -Dtools.finalName=geowave-restservices-${GEOWAVE_VERSION}-${VENDOR_VERSION}-accumulo1.7 $BUILD_ARGS "$@" -Daccumulo.version=1.7.2 -Daccumulo.api=1.7
