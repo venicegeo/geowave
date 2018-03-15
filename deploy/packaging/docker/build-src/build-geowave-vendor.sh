d105e0e8145d4af990d53d80b4b061a6b44a45da
Samuel Lightsey <samuel.lightsey@radiantsolutions.com>
2018-03-15 14:13:08 -0600

    test 2

--- deploy/packaging/docker/build-src/build-geowave-vendor.sh
+++ deploy/packaging/docker/build-src/build-geowave-vendor.sh
@@ -63,7 +63,7 @@ mvn -q package -am -pl deploy -P geotools-container-singlejar -Dgeotools.finalNa
 
 mvn -q package -am -pl services/rest -P rest-services-war -Drestservices.finalName=geowave-restservices-${GEOWAVE_VERSION}-${VENDOR_VERSION} $BUILD_ARGS "$@"
 
-mvn -q package -am -pl services/grpc -P grpc-singlejar -Drestservices.finalName=geowave-grpc-${GEOWAVE_VERSION}-${VENDOR_VERSION} $BUILD_ARGS "$@"
+mvn -q package -am -pl services/grpc/server -P grpc-singlejar -Drestservices.finalName=geowave-grpc-${GEOWAVE_VERSION}-${VENDOR_VERSION} $BUILD_ARGS "$@"
 
 mvn -q package -am -pl deploy -P accumulo-container-singlejar -Daccumulo.finalName=geowave-accumulo-${GEOWAVE_VERSION}-${VENDOR_VERSION} $BUILD_ARGS "$@"
 
