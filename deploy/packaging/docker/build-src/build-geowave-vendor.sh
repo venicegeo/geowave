229cc9b690b05f7c8cc3dd30be983cfddef518d2
Samuel Lightsey <samuel.lightsey@radiantsolutions.com>
2018-04-30 08:47:21 -0600

    fix name

--- deploy/packaging/docker/build-src/build-geowave-vendor.sh
+++ deploy/packaging/docker/build-src/build-geowave-vendor.sh
@@ -69,7 +69,7 @@ mvn -q package -am -pl deploy -P hbase-container-singlejar -Dhbase.finalName=geo
 
 mvn -q package -am -pl deploy -P geowave-tools-singlejar -Dtools.finalName=geowave-tools-${GEOWAVE_VERSION}-${VENDOR_VERSION} $BUILD_ARGS "$@"
 
-mvn -q package -am -pl deploy -P geowave-grpc-singlejar -Dtools.finalName=geowave-tools-${GEOWAVE_VERSION}-${VENDOR_VERSION} $BUILD_ARGS "$@"
+mvn -q package -am -pl deploy -P geowave-grpc-singlejar -Dtools.finalName=geowave-grpc-${GEOWAVE_VERSION}-${VENDOR_VERSION} $BUILD_ARGS "$@"
 
 # Build Accumulo API Jars
 if [[ "$ACCUMULO_API" != "1.7" ]]; then
